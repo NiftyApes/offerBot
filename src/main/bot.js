00799c4bd7d5a7d55396f8f9096da958fc7677bb
zishansami102 <zishansami102@gmail.com>
2023-04-28 21:07:00 +0530

    script fix

--- src/main/bot.js
+++ src/main/bot.js
@@ -68,18 +68,18 @@ export const bot = async function (input) {
             for (let i = 0; i < allMakerLoans.length; i++) {
                 const loan = allMakerLoans[i];
                 if (loan.loan.periodEndTimestamp <= blockTimestamp) {
-                    printLog("Expired: ", "0xe08319d9D09d098D06194ce6420754bB80640E16", 11);
+                    printLog("Expired: " + loan.offer.offer.nftContractAddress +":" +String(loan.offer.offer.nftId));
 
-                    const topBid = await getTopWethOffer(chainId, "0xe08319d9D09d098D06194ce6420754bB80640E16", 11);
+                    const topBid = await getTopWethOffer(chainId, loan.offer.offer.nftContractAddress, loan.offer.offer.nftId);
                     if(topBid) {
                         validTopBids.push({
-                            nftContractAddress: "0xe08319d9D09d098D06194ce6420754bB80640E16",
-                            nftId: 11,
+                            nftContractAddress: loan.offer.offer.nftContractAddress,
+                            nftId: loan.offer.offer.nftId,
                             encodedTopBid: encodeRawSeaportOrderData(topBid.rawData)
                         });
                     }    
                 } else {
-                    printLog("Active: ", "0xe08319d9D09d098D06194ce6420754bB80640E16", 11);
+                    printLog("Active: ", loan.offer.offer.nftContractAddress, loan.offer.offer.nftId);
                 }
             }   
 
